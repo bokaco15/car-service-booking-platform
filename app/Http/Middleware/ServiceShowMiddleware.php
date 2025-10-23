9f56e2ec864ffebeac46e91c66553299b2c17ed1
bokaco15 <bborislavilic@gmail.com>
2025-10-23 08:00:39 +0200

    Changed navbar routeIs status

--- app/Http/Middleware/ServiceShowMiddleware.php
+++ app/Http/Middleware/ServiceShowMiddleware.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class ServiceShowMiddleware
@@ -15,8 +17,10 @@ class ServiceShowMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->user()->hasRole('admin')) {
-            return $next($request);
+        if (Auth::check()) {
+            if (auth()->user()->hasRole('admin')) {
+                return $next($request);
+            }
         }
         if ($request->route('service')) {
             $service = $request->route('service');
