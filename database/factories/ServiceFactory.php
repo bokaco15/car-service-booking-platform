683c47cd4955b68a2e923df9ef80e724b83d8320
bokaco15 <bborislavilic@gmail.com>
2025-10-24 09:25:37 +0200

    * Added service status enum to service factory

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\ServiceStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Service>
+ */
+class ServiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 1,
+            'name' => fake()->name(),
+            'city' => fake()->city(),
+            'description' => fake()->text(30),
+            'status' => fake()->randomElement(ServiceStatus::cases()),
+        ];
+    }
+}
