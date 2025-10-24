cb805d7997b6760334d1e8744fe9a69ffedbadf5
bokaco15 <bborislavilic@gmail.com>
2025-10-24 09:47:59 +0200

    * commit

--- database/migrations/2025_10_11_191633_create_service_offerings_table.php
+++ database/migrations/2025_10_11_191633_create_service_offerings_table.php
@@ -21,6 +21,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('service_offerings');
+        Schema::dropIfExists(ServiceOffering::TABLE);
     }
 };
