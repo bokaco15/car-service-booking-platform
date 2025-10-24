9b621be0281a0848b7568813192c3ce4de75f8f7
bokaco15 <bborislavilic@gmail.com>
2025-10-24 09:03:56 +0200

    * Added const service table name * Used new syntax for foreign_id

--- database/migrations/2025_10_11_191633_create_service_offerings_table.php
+++ database/migrations/2025_10_11_191633_create_service_offerings_table.php
@@ -1,24 +1,17 @@
 <?php
 
+use App\Models\ServiceOffering;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('service_offerings', function (Blueprint $table) {
+        Schema::create(ServiceOffering::TABLE, function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('service_id');
-                $table->foreign('service_id')
-                    ->references('id')
-                    ->on('services')
-                    ->onUpdate('cascade')
-                    ->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('name', 255);
             $table->unsignedSmallInteger('duration_minutes');
             $table->unsignedMediumInteger('price');
@@ -26,9 +19,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('service_offerings');
