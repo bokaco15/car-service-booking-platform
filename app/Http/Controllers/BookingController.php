28d230d74910f3a4ccde1150d29ab8c06db93922
bokaco15 <bborislavilic@gmail.com>
2025-11-04 14:41:43 +0100

    BookingController update Requests for booking controller

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,57 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\BookingStatus;
+use App\Http\Requests\BookingInsertRequest;
+use App\Http\Requests\BookingStatusUpdate;
 use App\Models\Booking;
 use App\Models\Service;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class BookingController extends Controller
 {
-    public function insert(Request $request)
+    public function insert(BookingInsertRequest $request): RedirectResponse
     {
-        $request->validate([
-           'service_offering_id' => 'required|integer',
-            'start_at' => 'required',
-            'end_at' => 'required'
-        ]);
-
-        Booking::create([
-            'service_id' => $request->service_id,
-            'client_id' => Auth::user()->id,
-            'service_offering_id' => $request->service_offering_id,
-            'start_at' => $request->start_at,
-            'end_at' => $request->end_at
-        ]);
-
+        Booking::create(array_merge(
+            $request->validated(),
+            ['client_id'=>Auth::id()]
+            ));
         return redirect()->back()->with('booking_success', 'You have been successfully made reservation');
     }
 
-    public function show(Service $service)
+    public function show(Service $service): View
     {
         $bookings = Booking::with('service', 'serviceOffering')->where('service_id', $service->id)->get();
         $service_name=$service->name;
         $counter = 0;
-        if ($bookings->isEmpty()) {
-            return abort(403, 'you dont have bookings');
-        }
         return view('booking.show', compact('bookings', 'service_name', 'counter'));
     }
 
-    public function edit(Booking $booking)
+    public function edit(Booking $booking): View
     {
-        //if uslovi ako je vlasnik ili admin!
-        $statuses = [
-            'pending',
-            'confirmed',
-            'rejected',
-            'cancelled',
-            'done'
-        ];
+        $statuses = BookingStatus::cases();
         return view('booking.edit', compact('booking', 'statuses'));
     }
 
-    public function update(Request $request, Booking $booking)
+    public function update(BookingStatusUpdate $request, Booking $booking): RedirectResponse
     {
         $booking->status = $request->status;
         $booking->save();
@@ -60,7 +45,7 @@ class BookingController extends Controller
         return redirect()->route('booking.show', $booking->service->id)->with('update-success', 'You have been successfuly update status');
     }
 
-    public function delete(Booking $booking)
+    public function delete(Booking $booking): RedirectResponse
     {
         $booking->delete();
         return redirect()->route('booking.show', $booking->service->id)->with('delete-success', 'You have been successfuly delete booking');
