452c5765839bb538055c23e1bc5c97a1181c6e93
bokaco15 <bborislavilic@gmail.com>
2025-11-04 11:43:40 +0100

    optimized imports

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Booking;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class BookingSeeder extends Seeder
