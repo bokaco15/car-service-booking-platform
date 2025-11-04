1187d54a1fb45ddbebb6b5e36470ee41df77344c
bokaco15 <bborislavilic@gmail.com>
2025-11-04 15:16:08 +0100

    Service controller and requests updated

--- app/Http/Controllers/ServiceOfferingController.php
+++ app/Http/Controllers/ServiceOfferingController.php
@@ -6,39 +6,37 @@ use App\Http\Requests\ServiceOfferingAddRequest;
 use App\Models\Service;
 use App\Models\ServiceOffering;
 use App\Repositories\ServiceOfferingRepository;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ServiceOfferingController extends Controller
 {
-    private $serviceOfferingRepo;
+    public function __construct(private ServiceOfferingRepository $serviceOfferingRepo){}
 
-    public function __construct()
-    {
-        $this->serviceOfferingRepo = new ServiceOfferingRepository();
-    }
-
-    public function add(Service $offer)
+    public function add(Service $offer): View
     {
         return view('serviceOffering.add', compact('offer'));
     }
 
-    public function insert(ServiceOfferingAddRequest $request, Service $offer)
+    public function insert(ServiceOfferingAddRequest $request, Service $offer): RedirectResponse
     {
         $this->serviceOfferingRepo->insert($request, $offer);
         return redirect()->route('service.show', $offer->id)->with('success', 'You have been successfully added an offer');
     }
 
-    public function delete(ServiceOffering $offer)
+    public function delete(ServiceOffering $offer): RedirectResponse
     {
         $offer->delete();
         return redirect()->back();
     }
 
-    public function edit(ServiceOffering $offer){
+    public function edit(ServiceOffering $offer): View
+    {
         return view('serviceOffering.edit', compact('offer'));
     }
 
-    public function update(Request $request, ServiceOffering $offer)
+    public function update(Request $request, ServiceOffering $offer): RedirectResponse
     {
         $offer->name = $request->name;
         $offer->duration_minutes = $request->duration_minutes;
